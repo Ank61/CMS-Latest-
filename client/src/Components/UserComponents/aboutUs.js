a7c8d3181df56d709d61ad00fc2ea22eabfae884
Ankit Chohan <Sidhantuppal08@gmail.com>
2023-04-28 09:29:30 +0530

    form validation

--- client/src/Components/UserComponents/aboutUs.js
+++ client/src/Components/UserComponents/aboutUs.js
@@ -1,154 +1,175 @@
-import React,{ useEffect, useState,useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "../Common/SecureInstance/axiosInstance";
 import { useNavigate } from "react-router";
-import  Helmet  from 'react-helmet';
+import Helmet from 'react-helmet';
 import Header from "../Common/Header/header";
 import parse, { domToReact } from 'html-react-parser';
 import './about.css';
 import networkConstant from "../Common/API/uri_constant";
 
-function AboutUs(){
-    const [data,setData] = useState()
-    const [title , setTitle] = useState("Initial Title")
-    const [description, setDescription] = useState("Initial Description")
-    const navigate = useNavigate()
-    const [allInputs , setAllInputs] = useState("");
-    const [emptyInputs , setEmptyInputs] = useState(false)
+function AboutUs() {
+  const [data, setData] = useState()
+  const [title, setTitle] = useState("Initial Title")
+  const [description, setDescription] = useState("Initial Description")
+  const navigate = useNavigate()
+  const [allInputs, setAllInputs] = useState("");
+ // const [InputArray, setInputArray] = useState([])
 
-
-var Array = []
-let numInputs = 0;
-const inputRefs = [];
-const handleClick =(e ,route )=>{
-  console.log("button clicked" , e.target.value);
-  navigate(route)
-}
-const handleClickNotfocus = (e ,route)=>{
-  debugger;
-  console.log("button clicked" , e.target.value);
-  navigate(route)
-}
-const handleSubmitButton = (e)=>{
-console.log("Clicked" ,allInputs)
-var getAllString = allInputs.split(';');
-var fullString = allInputs;
-var getItem,lastOccurrenceIndex ,getStartingIndex, finalFirst,splitFinal ;
-for(let i=0;i<numInputs;i++){
-  if(i===0){
-    getItem = 3;
-    getStartingIndex = getAllString[getItem];
-    lastOccurrenceIndex  = fullString.lastIndexOf(getStartingIndex);
-    finalFirst = fullString.slice(lastOccurrenceIndex);
-    splitFinal = finalFirst.split(";");
-    //setFinalArray((prev)=>[...prev, finalArray.push( splitFinal[0])])
-   console.log( splitFinal[0]);
-   Array.push( splitFinal[0]);
-  console.log(splitFinal)
-  }else{
-    //for i=1 && i=2 and more...
-    getStartingIndex = splitFinal.slice(i);
-    lastOccurrenceIndex = fullString.lastIndexOf(getStartingIndex[0+i]);
-    finalFirst = fullString.slice(lastOccurrenceIndex);
-    splitFinal = finalFirst.split(";");
-    console.log("In the else part" , splitFinal[0]);
-    Array.push( splitFinal[0]);
+  var Array = []
+  let numInputs = 0;
+  var InputCounter = 0;
+  var isEmptyOrWhitespace;
+  var ShowError=0;
+  const handleClick = (e, route) => {
+    navigate(route)
   }
-}
-console.log("Final Array" ,Array)
-axios.post(networkConstant.URL.submitButton , Array).then(response=>console.log(response)).catch(err=>console.log(err))
-
-const inputFields = document.querySelectorAll("input");
-inputFields.forEach(input => {
-  input.value = '';
-});
-
-}
-const handleAllInput = (e)=>{
-  setAllInputs((prev)=>prev + ';' +e.target.value)
-}
-const options = {
-  replace: ({ attribs, children }) => {
-    if (!attribs) {
-      return;
-    }
-    if (attribs.id === 'buttonRoute') {
-      console.log(attribs.style )
-      return React.createElement(
-        'button',
-       { onClick: (e)=>handleClick(e,attribs.value),
-      className : attribs.class,},
-        domToReact(children, options)
-      )
-    }
-    if (attribs.class === 'NonFocus') {
-      return React.createElement(
-        'button',
-       { onClick: (e)=>handleClickNotfocus(e,attribs.value),
-       },
-        domToReact(children, options)
-      )
-    }
-    if(attribs.id==="postIt"){
-      return React.createElement(
-        'button',
-        {onClick : (e)=>handleSubmitButton(e),
-        className : attribs.class,
-      style:{}},
-        domToReact(children, options)
-      )
-    }
-    if(attribs.class==="form-control"){
-      numInputs++;
-      return React.createElement(
-        'input',
-        {onChange : (e)=>handleAllInput(e),
-        className : attribs.class},
-      )
-    }
-    if (attribs.class === 'prettify') {
-      return React.createElement(
-        'span',
-        { style: { color: 'hotpink' } },
-        domToReact(children, options)
-      );
-    }
+  const handleClickNotfocus = (e, route) => {
+    navigate(route)
   }
-};
-
-const reactElement = parse(`${data}`, options);
-
-    useEffect(()=>{
-        axios.get(`${networkConstant.URL.userAboutUs}`)
-        .then(response=>{
-            if (response.data ==="Logout") {
-                navigate("/admin")
-            } else {
-              setTitle(response.data.rest[0].title)
-              setDescription(response.data.rest[0].description)
-              setData(response.data.data)
-            }
-        })
-        .catch(err=>console.log(err))
+  const handleSubmitButton = (e) => {
+    const checkFields = document.querySelectorAll("input");
+    checkFields.forEach((input, index) => {
+      const regex = /^\s*$/;
+      isEmptyOrWhitespace = regex.test(input.value);
+      if (isEmptyOrWhitespace===true) {
+        console.log("In the error",index)
+        var elementId = document.getElementById(input.id);
+        elementId.style.backgroundColor = "red";
+        ShowError=1;
+        return
+        //setInputArray((prev) => ([...prev, { name: input.id, Index: index, isEmpty: true }]))
+      }
+      //   setInputArray((prev)=>([...prev, { name: input.id, Index: index, isEmpty: false }]))
+      })
+      if(ShowError===0){
+      console.log("Reached")
+      var getAllString = allInputs.split(';');
+        var fullString = allInputs;
+        var getItem, lastOccurrenceIndex, getStartingIndex, finalFirst, splitFinal;
+        for (let i = 0; i < numInputs; i++){
+          if (i === 0) {
+            getItem = 3;
+            getStartingIndex = getAllString[getItem];
+            lastOccurrenceIndex = fullString.lastIndexOf(getStartingIndex);
+            finalFirst = fullString.slice(lastOccurrenceIndex);
+            splitFinal = finalFirst.split(";");
+            console.log(splitFinal[0]);
+            Array.push(splitFinal[0]);
+            console.log(splitFinal)
+          } else {
+            getStartingIndex = splitFinal.slice(i);
+            lastOccurrenceIndex = fullString.lastIndexOf(getStartingIndex[0 + i]);
+            finalFirst = fullString.slice(lastOccurrenceIndex);
+            splitFinal = finalFirst.split(";");
+            Array.push(splitFinal[0]);
+          }
+        }
+        axios.post(networkConstant.URL.submitButton, Array).then(response => console.log(response)).catch(err => console.log(err))
+        const inputFields = document.querySelectorAll("input");
+        inputFields.forEach(input => {
+          input.value = '';
+        });
+      }
     }
-    ,[])
+  
+  const handleRequiredInput = (e, id) => {
+    var element = document.getElementById(id)
+    element.style.backgroundColor = "white";
+  }
+  const handleAllInput = (e) => {
+    setAllInputs((prev) => prev + ';' + e.target.value)
+  }
+  const options = {
+    replace: ({ attribs, children }) => {
+      if (!attribs) {
+        return;
+      }
+      if (attribs.id === 'buttonRoute') {
+        return React.createElement(
+          'button',
+          {
+            onClick: (e) => handleClick(e, attribs.value),
+            className: attribs.class,
+          },
+          domToReact(children, options)
+        )
+      }
+      if (attribs.class === 'NonFocus') {
+        return React.createElement(
+          'button',
+          {
+            onClick: (e) => handleClickNotfocus(e, attribs.value),
+          },
+          domToReact(children, options)
+        )
+      }
+      if (attribs.id === "postIt") {
+        return React.createElement(
+          'button',
+          {
+            onClick: (e) => handleSubmitButton(e),
+            className: attribs.class,
+            style: { fontSize: 13, marginTop: 20, marginLeft: '40%', marginRight: '40%' }
+          },
+          domToReact(children, options)
+        )
+      }
+      if (attribs.required === "true") {
+        return <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '7%' }}>
+          <input type="text" className="form-control" onChange={(e) => handleRequiredInput(e, attribs.id)} style={{ height: 30, marginRight: '10%', width: 'auto' }} id={attribs.id}></input>
+        </div>
 
+      }
+      if (attribs.class === "form-control") {
+        numInputs++;
+        return React.createElement(
+          'input',
+          {
+            onChange: (e) => handleAllInput(e),
+            className: attribs.class
+          },
+        )
+      }
+      if (attribs.class === 'prettify') {
+        return React.createElement(
+          'span',
+          { style: { color: 'hotpink' } },
+          domToReact(children, options)
+        );
+      }
+    }
+  };
+  const reactElement = parse(`${data}`, options);
+  useEffect(() => {
+    axios.get(`${networkConstant.URL.userAboutUs}`)
+      .then(response => {
+        if (response.data === "Logout") {
+          navigate("/admin")
+        } else {
+          setTitle(response.data.rest[0].title)
+          setDescription(response.data.rest[0].description)
+          setData(response.data.data)
+        }
+      })
+      .catch(err => console.log(err))
+  }
+    , [])
 
-
-return (
+  return (
     <div>
-          <Helmet 
-        title = {`${title}`}
+      <Helmet
+        title={`${title}`}
         meta={[
-        {
-          name: `description`,
-          content: description,
-        } 
-      ]}/>
-      <Header/>
-        {reactElement}
-        {/* {data? <div dangerouslySetInnerHTML={{ __html: data }}></div> : ""} */}
-        {/* {JSON.stringify(data)} */}
+          {
+            name: `description`,
+            content: description,
+          }
+        ]} />
+      <Header />
+      {reactElement}
+      {/* {data? <div dangerouslySetInnerHTML={{ __html: data }}></div> : ""} */}
+      {/* {JSON.stringify(data)} */}
     </div>
-)
+  )
 }
 export default AboutUs;
